3f61da3f35fceb5e265fc02a1472d4f1e07c9ff3
aurelie <aurelie.cissokho@gmail.com>
2019-10-29 16:28:48 +0100

    modification patient class

--- src/main/java/fr/wcs/doctors/consultation/entities/Patient.java
+++ src/main/java/fr/wcs/doctors/consultation/entities/Patient.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "PATIENT")
 public class Patient implements Serializable {
 
 	private static final long serialVersionUID = 1L;
